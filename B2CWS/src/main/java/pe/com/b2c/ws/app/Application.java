8a38ce0091e8cdf1eb9fd2d1b391e6bb232bd0b8
Jose <josec8080@gmail.com>
2015-05-12 16:20:12 -0500

    Estructura inicial del proyecto

---
+++ B2CWS/src/main/java/pe/com/b2c/ws/app/Application.java
@@ -0,0 +1,20 @@
+package pe.com.b2c.ws.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
