f87921d217bdb276752a7c009a7126b6d2902a0d
Jose <josec8080@gmail.com>
2015-05-12 18:06:37 -0500

    Agregadas clases utilitarias

---
+++ B2CUtil/src/main/java/pe/com/b2c/util/SystemException.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.com.b2c.util;
+
+/**
+ *
+ * @author Jose
+ */
+public class SystemException extends Exception{
+    public SystemException() {
+    }
+
+    public SystemException(String message) {
+        super(message);
+    }
+
+    public SystemException(Throwable cause) {
+        super(cause);
+    }
+
+    public SystemException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ B2CUtil/src/main/java/pe/com/b2c/util/SystemUtil.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.com.b2c.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author Jose
+ */
+public class SystemUtil {
+    //Constructor privado para que nadie pueda instanciar la clase
+    private SystemUtil(){
+        
+    }
+    //Metodo para obtener informacion de un archivo .properties
+    public static String obtenerPropiedad(String archivo, String clave){
+        ResourceBundle rb = ResourceBundle.getBundle(archivo);
+        return rb.getString(clave);
+    }
+    
+    //Metodo para formatear fechas
+    public static String convertirDate(Date fecha, String formato){
+        SimpleDateFormat sdf = new SimpleDateFormat(formato);
+        return sdf.format(fecha);
+    }
+    
+    public static String idError(){
+        return convertirDate(new Date(), "ddMMyyyyhhmmss");
+    }
+    
+    //Metodo para obtener una variable del sistema
+    public static String obtenerVariableSistema(String clave){
+        return System.getProperty(clave);
+    }
+    
+    public static String validaNulo(String texto){
+        return texto == null ? "" : texto;
+    }
+    
+    
+}
